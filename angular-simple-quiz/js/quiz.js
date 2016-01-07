d7ec385be629e156863ab2168651472becfbdadf
Martin Gomola <martin.gomola@gmail.com>
2016-01-07 17:44:18 +0100

    no message

--- angular-simple-quiz/js/quiz.js
+++ angular-simple-quiz/js/quiz.js
@@ -1,5 +1,30 @@
-(function(){
+(function() {
+    /**
+     * myQuiz Module
+     *
+     * Description
+     */
+    var app = angular.module('myQuiz', []);
 
-	// script goes here
+    app.controller('QuizCtrl', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
+
+        $scope.score = 0;
+        $scope.activeQuestion = -1;
+        $scope.activeQuestionAnswered = 0;
+        $scope.percentage = 0;
+
+        $http.get('quiz_data.json').then(function(quizData) {
+            $scope.myQuestions = quizData.data;
+            $scope.totalQuestions = $scope.myQuestions.length;
+
+
+
+        });
+
+
+
+
+    }])
+
 
 })();
